18e9b4ede6dc34835c0b67a669f7b4b646a169fc
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-26 07:40:46 -0600

    Update main2.py

--- main2.py
+++ main2.py
@@ -6,3 +6,9 @@ from livekit.plugins import openai, silero
 from api import AssistantFnc
 
 load_dotenv()
+async def entrypoint(ctx: JobContext):
+    initial_ctx = llm.ChatContext().append(
+        role="system",
+        text=(
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
